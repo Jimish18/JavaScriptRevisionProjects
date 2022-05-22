7eb4f9366dfd402a49026e4ebb1bbd65842ce01d
jimish1806 <jimish957@gmail.com>
2022-05-22 19:23:10 +0530

    sending HTTPrequest

--- ajax/app.js
+++ ajax/app.js
@@ -1,1 +1,43 @@
 console.log("Jay shree Ram");
+
+let data;
+
+let fetchData = document.getElementById("fetchData");
+let popData = document.getElementById("popData");
+
+fetchData.addEventListener("click",fetchDataHandler);
+
+function fetchDataHandler()
+{
+    console.log("we are fetching data");
+
+    let xhr = new XMLHttpRequest();
+
+    // On Open XHR GET request 
+    xhr.open("GET","jimish.txt",true);
+    // for POST request
+    // xhr.getResponseHeader('Content-type','application/json');
+
+    xhr.onprogress = () =>
+    {
+        console.log("we are processing the data");
+    }
+
+    xhr.onload = function()
+    {
+        data = this.responseText;
+        console.log(data);
+    }
+
+    console.log(data);
+
+    // let para = `I like singing , and travelling. `
+
+    xhr.send();
+
+    console.log("we are done now...!");
+}
+
+// popData.addEventListener
+
+console.log(data);
