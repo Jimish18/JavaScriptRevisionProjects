77df58011958924f80da7f236a38f57cdb3e97bc
jimish1806 <jimish957@gmail.com>
2022-05-23 19:45:49 +0530

    stuck on invoke different functions for AJAX call

--- ajax/app.js
+++ ajax/app.js
@@ -1,43 +1,103 @@
 console.log("Jay shree Ram");
 
-let data;
-
 let fetchData = document.getElementById("fetchData");
-let popData = document.getElementById("popData");
+// let popData = document.getElementById("popData");
+let titleData  = document.getElementById("titleData");
+let contentData = document.getElementById("contentData");
 
-fetchData.addEventListener("click",fetchDataHandler);
+// fetchData.addEventListener("click",fetchDataHandler);
 
-function fetchDataHandler()
+// function fetchDataHandler()
+// {
+//     console.log("we are fetching data");
+
+//     let xhr = new XMLHttpRequest();
+
+//     // On Open XHR GET request 
+//     xhr.open("GET","jimish.txt",true);
+//     // for POST request
+//     // xhr.getResponseHeader('Content-type','application/json');
+
+//     xhr.onprogress = () =>
+//     {
+//         console.log("we are processing the data");
+//     }
+
+//     xhr.onload = function()
+//     {
+//         console.log(this.responseText);
+//     }
+
+//     xhr.send();
+
+//     console.log("we are done now...!");
+// }
+
+// popData.addEventListener("click",popDatafunc);
+
+// function popDatafunc()
+// {
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.open("GET","https://jsonplaceholder.typicode.com/posts",true);
+
+//     xhr.onprogress = () =>
+//     {
+//         console.log("we are fetching data for you...!");
+//     }
+    
+//     xhr.onload = function()
+//     {
+//         if(this.status === 200)
+//         {
+//             let obj = JSON.parse(this.responseText);
+
+//             console.log(obj);
+
+//             let dataList = document.getElementById("dataList");
+
+//             str = ``;
+
+//             let size = Object.keys(obj).length;
+
+//             for(let i = 0 ; i < size; i++)
+//             {
+//                 str += `<li>${obj[i].title}</li>`;
+//             }
+
+//             dataList.innerHTML = str;
+//         }
+//     }
+
+//     xhr.send();
+
+//     console.log("We're done with fetching your data");
+// }
+
+// titleData.addEventListener("click",loadData(`https://jsonplaceholder.typicode.com/posts`,fetchTitleFunction));
+
+function loadData(url,cFunction)
 {
-    console.log("we are fetching data");
+    const xhr = new XMLHttpRequest();
 
-    let xhr = new XMLHttpRequest();
-
-    // On Open XHR GET request 
-    xhr.open("GET","jimish.txt",true);
-    // for POST request
-    // xhr.getResponseHeader('Content-type','application/json');
-
-    xhr.onprogress = () =>
-    {
-        console.log("we are processing the data");
-    }
+    xhr.open("GET",url,true);
 
     xhr.onload = function()
     {
-        data = this.responseText;
-        console.log(data);
+        cFunction(this);
     }
 
-    console.log(data);
-
-    // let para = `I like singing , and travelling. `
-
     xhr.send();
-
-    console.log("we are done now...!");
 }
 
-// popData.addEventListener
+function fetchTitleFunction(xhttp)
+{
+    // let obj = JSON.parse(xhttp.responseText);
+    // let size = Object.keys()
+    console.log("hi");
+}
 
-console.log(data);
+// function fetchContentFunction(xhttp)
+// {
+    
+// }
